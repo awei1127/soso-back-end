4d5f4dd9c805028ced8adc5cd75debedfa547709
Wei <awei1127@hotmail.com>
2023-07-17 19:38:15 +0800

    add models and set database's name to create tables in database

---
+++ migrations/20230717104602-create-product.js
@@ -0,0 +1,46 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      owner_id: {
+        type: Sequelize.INTEGER
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      public: {
+        type: Sequelize.BOOLEAN
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products')
+  }
+}
---
+++ models/cartitem.js
@@ -0,0 +1,27 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class CartItem extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  CartItem.init({
+    userId: DataTypes.INTEGER,
+    productId: DataTypes.INTEGER,
+    checked: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'CartItem',
+    tableName: 'CartItems',
+    underscored: true
+  })
+  return CartItem
+}
