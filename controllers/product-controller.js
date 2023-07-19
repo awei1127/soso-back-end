9d09571b3f694d77ebcdfedd035b70c05867b688
Wei <awei1127@hotmail.com>
2023-07-19 22:43:47 +0800

    add getProducts API

---
+++ controllers/product-controller.js
@@ -0,0 +1,55 @@
+const { Product, Category } = require('../models')
+const { Op } = require('sequelize')
+
+const productController = {
+  getProducts: async (req, res, next) => {
+    // 取得查詢參數
+    const { categoryId, lowestPrice, highestPrice, keyword, sortBy } = req.query
+    const query = {}
+    const order = []
+    if (highestPrice) {
+      query.price = { [Op.lte]: highestPrice }
+    }
+    if (lowestPrice) {
+      query.price = { [Op.gte]: lowestPrice }
+    }
+    if (!sortBy || sortBy === 'new') {
+      order.push(['createdAt', 'DESC'])
+    } else if (sortBy === 'highprice') {
+      order.push(['price', 'DESC'])
+    } else if (sortBy === 'lowprice') {
+      order.push(['price', 'ASC'])
+    } else {
+      order.push(['createdAt', 'DESC'])
+    }
+    // 取得商品資料
+    try {
+      if (keyword) { // 如果關鍵字有值，查詢關鍵字
+        query.name = { [Op.like]: `%${keyword}%` }
+        const products = await Product.findAll({
+          where: query,
+          order
+        })
+        return res.json({ status: 'success', data: products })
+      } else if (categoryId) { // 如果分類ID有值，查詢分類ID
+        const products = await Product.findAll({
+          where: query,
+          include: [
+            {
+              model: Category,
+              attributes: ['id'],
+              as: 'Categories',
+              where: { id: categoryId }
+            }
+          ],
+          order
+        })
+        return res.json({ status: 'success', data: products })
+      }
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = productController
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const passport = require('passport')
 const authenticated = require('../middleware/auth')
 const userController = require('../controllers/user-controller')
 const cartController = require('../controllers/cart-controller')
+const productController = require('../controllers/product-controller')
 // const { errorHandler } = require('../middleware/error-handler')
 // const upload = require('../middleware/multer')
 
-router.post('/api/v1/signin', passport.authenticate('local', { session: false }), userController.signIn)
+router.get('/api/v1/products', productController.getProducts)
 router.post('/api/v1/signup', userController.signUp)
+router.post('/api/v1/signin', passport.authenticate('local', { session: false }), userController.signIn)
 router.get('/api/v1/cart/:userId', authenticated, cartController.getCartItems)
 
 module.exports = router
