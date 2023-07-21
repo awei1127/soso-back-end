a46e2a35f76dd877ce36c4ba35780cb4294a422a
Wei <awei1127@hotmail.com>
2023-07-21 19:05:14 +0800

    show product information when user look their cart items

--- controllers/cart-controller.js
+++ controllers/cart-controller.js
@@ -1,11 +1,18 @@
-const { CartItem } = require('../models')
+const { CartItem, Product } = require('../models')
 
 const cartController = {
   getCartItems: async (req, res, next) => {
     // 用userId取得該使用者的購物車物品清單(cartItems)
     const userId = req.user.id
     try {
-      const cartItems = await CartItem.findAll({ where: { userId } })
+      const cartItems = await CartItem.findAll({
+        where: { userId },
+        include: [
+          {
+            model: Product
+          }
+        ]
+      })
       return res.json({ status: 'success', data: cartItems })
     } catch (err) {
       next(err)
