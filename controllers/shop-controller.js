ba7fb3bb1c04df2e83dfa82694bfca26425bb7d4
Wei <awei1127@hotmail.com>
2023-07-26 01:25:59 +0800

    fix bug: when add product, add data to product table and also productcategory table

--- controllers/shop-controller.js
+++ controllers/shop-controller.js
@@ -1,4 +1,4 @@
-const { Product } = require('../models')
+const { sequelize, Product, ProductCategory } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const shopController = {
@@ -16,31 +16,44 @@ const shopController = {
     // 新增商品
     const { file } = req
     const userId = req.user.id
-    const { name, description } = req.body
+    const { name, description, categoryId } = req.body
     let { price, stock, isPublic } = req.body
     let filePath
+
     if (!name || !price || !stock || !description || !isPublic) {
       return res.status(400).json({ status: 'failure', message: 'All required.' })
     }
-    // 把multer處理過的form-data資料轉換成期望的型別
+
     try {
+      // 把multer處理過的form-data資料轉換成期望的型別
       isPublic = JSON.parse(isPublic)
       price = Number(price)
       stock = Number(stock)
-    } catch (err) {
-      next(err)
-    }
-    // 檢查price和stock是否大於0且是整數
-    if (price <= 0 || stock <= 0 || !Number.isInteger(price) || !Number.isInteger(stock)) {
-      return res.status(400).json({ status: 'failure', message: 'Invalid value.' })
-    }
-    try {
+      // 檢查price和stock是否大於0且是整數
+      if (price <= 0 || stock <= 0 || !Number.isInteger(price) || !Number.isInteger(stock)) {
+        return res.status(400).json({ status: 'failure', message: 'Invalid value.' })
+      }
       if (file) {
         filePath = await imgurFileHandler(file)
       }
-      const product = await Product.create({ ownerId: userId, name, price, stock, description, isPublic, image: filePath })
-      return res.json({ status: 'success', data: product })
     } catch (err) {
+      next(err)
+    }
+
+    // 建立一個交易
+    const transaction = await sequelize.transaction()
+
+    try {
+      // 新增資料到Product表
+      const product = await Product.create({ ownerId: userId, name, price, stock, description, isPublic, image: filePath }, { transaction })
+      // 新增資料到ProductCategory表
+      await ProductCategory.create({ productId: product.id, categoryId }, { transaction })
+      // 如果一切正常，提交交易
+      await transaction.commit()
+      return res.json({ status: 'success', message: 'product added successfully' })
+    } catch (err) {
+      // 如果有任何錯誤發生，回滾交易
+      await transaction.rollback()
       next(err)
     }
   },
