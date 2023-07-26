d1e7bd13b6ebfecf214102614d4da90f90e7fca3
Wei <awei1127@hotmail.com>
2023-07-26 17:16:20 +0800

    fix bug: product-categories seed file

--- seeders/20230718185254-product-categories-seed-file.js
+++ seeders/20230718185254-product-categories-seed-file.js
@@ -4,16 +4,16 @@ const { Category, Product } = require('../models')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // 先取得目前資料庫中的categories跟products的數量
-    const countCategories = Category.count()
-    const countProducts = Product.count()
-    const [categoryAmount, productAmount] = await Promise.all([countCategories, countProducts])
+    // 先取得目前資料庫中的categories跟products
+    const categories = Category.findAll()
+    const products = Product.findAll()
+    const [categoryData, productData] = await Promise.all([categories, products])
 
     // 每個產品隨機分配category_id
     await queryInterface.bulkInsert('ProductCategories',
-      Array.from({ length: productAmount }, (_, i) => ({
-        product_id: i + 1,
-        category_id: [Math.floor(Math.random() * categoryAmount + 1)],
+      productData.map(product => ({
+        product_id: product.id,
+        category_id: categoryData[Math.floor(Math.random() * categoryData.length)].id,
         created_at: new Date(),
         updated_at: new Date()
       }))
